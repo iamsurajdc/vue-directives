1e19746ad7f7609b2400a876e7d8241005598049
iamsurajdc <surajchandgude0304@gmail.com>
2020-07-16 16:59:05 +0530

    Added basic custom directive

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 com-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Built in Directives</h1>
+        <p v-text="'Shambho'"></p>
+        <p v-html="'<strong>Strong Suraj</strong>'"></p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 com-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Custom Directives</h1>
+        <p v-highlight:background.delayed="'red'">Color This</p>
+        <p v-local-highlight:background.delayed="'red'">Color This, too</p>
+        <!-- <p v-html="'<strong>Strong Suraj</strong>'"></p> -->
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  directives: {
+    "local-highlight": {
+      bind(el, binding, vnode) {
+        console.log("bind -> el, binding, vnode", el, binding, vnode);
+        let delay = 0;
+
+        if (binding.modifiers["delayed"]) {
+          delay = 3000;
+          setTimeout(() => {
+            if (binding.arg) {
+              el.style.background = binding.value;
+            } else {
+              el.style.color = binding.value;
+            }
+          }, delay);
+        }
+      }
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.directive('highlight', {
+  bind(el, binding) {
+    // el.style.backgroundColor = binding.value;
+    let delay = 0;
+
+    if (binding.modifiers['delayed']) {
+      delay = 3000
+      setTimeout(() => {
+        if (binding.arg) {
+          el.style.background = binding.value
+        } else {
+          el.style.color = binding.value
+        }
+      }, delay);
+    }
+
+  }
+});
 
 new Vue({
   render: h => h(App),
