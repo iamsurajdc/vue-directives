fa5cba3e9415a6fbe182f6410d4b08a87e459fcb
iamsurajdc <surajchandgude0304@gmail.com>
2020-07-20 15:20:54 +0530

    added modifiers to the directive

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,14 @@
       <div class="col-xs-12 col-sm-8 com-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Built in Directives</h1>
         <p v-text="'Shambho'"></p>
-        <p v-html="'<strong>Strong Suraj</strong>'"></p>
+        <p v-html="'<strong>Strong text</strong>'"></p>
       </div>
     </div>
     <div class="row">
       <div class="col-xs-12 col-sm-8 com-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Custom Directives</h1>
+        <h1>Custom Directives: ;) </h1>
         <p v-highlight:background.delayed="'red'">Color This</p>
-        <p v-local-highlight:background.delayed="'red'">Color This, too</p>
+        <p v-local-highlight:background.delayed.blink="'red'">Color This, too</p>
         <!-- <p v-html="'<strong>Strong Suraj</strong>'"></p> -->
       </div>
     </div>
@@ -28,14 +28,32 @@ export default {
 
         if (binding.modifiers["delayed"]) {
           delay = 3000;
-          setTimeout(() => {
-            if (binding.arg) {
-              el.style.background = binding.value;
-            } else {
-              el.style.color = binding.value;
-            }
-          }, delay);
         }
+          if (binding.modifiers["blink"]) {
+            let mainColor = binding.value;
+            let secondColor = "blue";
+            let currentColor = mainColor;
+            setTimeout(() => {
+              setInterval(() => {
+                currentColor == secondColor
+                  ? (currentColor = mainColor)
+                  : (currentColor = secondColor);
+                if (binding.arg) {
+                  el.style.background = currentColor;
+                } else {
+                  el.style.color = binding.value;
+                }
+              }, 1000);
+            }, delay);
+          } else {
+            setTimeout(() => {
+              if (binding.arg == "background") {
+                el.style.background = binding.value;
+              } else {
+                el.style.color = binding.value;
+              }
+            }, delay);
+          }
       }
     }
   }
